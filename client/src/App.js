3556019ee7877306409abc2f449c558ec18fd1c5
SeanCassone <sean.cassone@gmail.com>
2021-09-03 23:20:26 -0400

    removed style

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import NavBar from "./components/NavBar/NavBar";
+import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/LoginPage/Login";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <LandingPage />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,33 @@
+import { useHistory, useLocation } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
+import { AddCircleOutlined, SubjectOutlined } from "@material-ui/icons";
 import useStyles from "./styles";
 import Avatar from "@material-ui/core/Avatar";
 import finnPlan from "../../images/FinnPlan.svg";
 const NavBar = () => {
+  const history = useHistory();
   const classes = useStyles();
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      text: "Login",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/login",
+    },
+    {
+      text: "Register",
+      icon: <AddCircleOutlined color="secondary" />,
+      path: "/signup",
+    },
+  ];
 
   return (
     <div className={classes.root}>
@@ -24,8 +44,21 @@ const NavBar = () => {
           <Typography variant="h6" className={classes.title}>
             Fin Plan
           </Typography>
-          <Button color="inherit">Register</Button>
-          <Button color="inherit">Login</Button>
+          <List>
+            {menuItems.map((item) => (
+              <ListItem
+                button
+                key={item.text}
+                onClick={() => history.push(item.path)}
+                className={
+                  location.pathname === item.path ? classes.active : null
+                }
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
         </Toolbar>
       </AppBar>
     </div>
